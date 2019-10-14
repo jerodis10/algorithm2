61d751927eec3c683ac82db1d5613d94408bcab7
jerodis10 <jerodis10@naver.com>
2019-10-14 10:41:27 +0900

    Create 1257. K번째 문자열.java

---
+++ 1257. K번째 문자열.java
@@ -0,0 +1,35 @@
+package sample;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static String originalWord;
+	static int T, K;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		
+		for(int tc=1; tc<=T; tc++) {
+			K = Integer.parseInt(br.readLine());
+			originalWord = br.readLine();
+			Set<String> allWords = new TreeSet<String>();
+			for(int i=1; i<=originalWord.length(); i++) {
+				for(int j=originalWord.length()-i; j>=0; j--) {
+					allWords.add(originalWord.substring(j, i+j));
+				}
+			}
+			List<String> sortedAllWords = new ArrayList<String>(allWords);
+			
+			if(K>sortedAllWords.size()) {
+				System.out.printf("#%s none%n", tc);
+			}
+			else {
+				System.out.printf("#%s %s%n", tc, sortedAllWords.get(K-1));
+			}
+
+		}
+	}
+}
+
