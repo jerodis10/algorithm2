5d5bdcde7280602616437494a00f2329847e352e
nth <jerodis10@naver.com>
2020-05-13 21:40:46 +0900

    수정

--- 3752_가능한시험점수.cpp
+++ 3752_가능한시험점수.cpp
@@ -1,53 +1,55 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int testCase, n;
+int testcase;
+int n;
 int num[101];
 bool visited[10001];
 
 vector<int> v;
 
 void mem_set(void* dest, int val, int size) {
-	unsigned char* p = (unsigned char*)dest;
-	while (size--) *p++ = val;
+    unsigned char* p = (unsigned char*)dest;
+    while (size--) *p++ = val;
 }
 
 int solve() {
-	v.push_back(0);
-	visited[0] = true;
+    visited[0] = true;
+    v.push_back(0);
 
-	for (int i = 0; i <= n; i++) {
-		int len = (int)v.size() - 1;
+    for (int i = 1; i <= n; i++) {
+        int len = (int)v.size() - 1;
 
-		for (int j = len; j >= 0; j--) {
-			int now = v[j];
-			int next = now + num[i];
+        for (int j = len; j >= 0; j--) {
+            int now = v[j];
+            int next = now + num[i];
 
-			if (!visited[next]) {
-				v.push_back(next);
-				visited[next] = true;
-			}
-		}
-	}
+            if (!visited[next]) {
+                v.push_back(next);
+                visited[next] = true;
+            }
+        }
+    }
 
-	return (int)v.size();
+    return (int)v.size();
 }
 
-
 int main() {
-	cin >> testCase;
-	for (int tc = 1; tc <= testCase; tc++) {
-		cin >> n;
-		for (int i = 1; i <= n; i++) {
-			cin >> num[i];
-		}
 
-		mem_set(visited, false, sizeof(visited));
-		v.clear();
+    cin >> testcase;
+    for (int tc = 1; tc <= testcase; tc++) {
+        cin >> n;
 
-		cout << "#" << testCase << " " << solve() << endl;
-	}
+        for (int i = 1; i <= n; i++) {
+            cin >> num[i];
+        }
 
-	return 0;
+        mem_set(visited, false, sizeof(visited));
+        cout << "#" << tc << " " << solve() << "\n";
+
+        v.clear();
+    }
+
+    return 0;
 }
