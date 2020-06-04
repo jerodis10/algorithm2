5450275f9cb007c8b4c54108bced70662cf901a8
nth <jerodis10@naver.com>
2020-06-04 22:12:17 +0900

    신규

--- 3074_입국심사.cpp
+++ 3074_입국심사.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
-#include <cstring>
-#include <algorithm>
 using namespace std;
 
-int testCase, n, ret, sum;
-int num[100001];
+typedef long long ll;
+int testCase, n, m;
+ll ret, maxNum;
+ll counter[100001];
 
-void mem_set(void* dest, int val, int size) {
-    unsigned char* p = (unsigned char*)dest;
-    while (size--) *p++ = val;
-}
-
-bool cmp(int a, int b) {
-    if (a > b) return true;
-    return false;
-}
-
-int main(int argc, const char* argv[]) {
+int main() {
     // cin,cout 속도향상
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
     cin >> testCase;
     for (int t = 1; t <= testCase; t++) {
-        mem_set(num, 0, sizeof(num));
-        sum = 0;
-        cin >> n;
+        ret = 0;
+        maxNum = 0;
+        cin >> n >> m;
         for (int i = 0; i < n; i++) {
-            cin >> num[i];
-            sum += num[i];
+            cin >> counter[i];
+            if (counter[i] > maxNum) maxNum = counter[i];
         }
-        sort(num, num + n, cmp);
-        for (int i = 0; i < n; i = i + 3) {
-            sum -= num[i + 2];
-        }
-        ret = sum;
+        
+        ll left = 0;
+        ll right = (ll)(maxNum * m);
+        ll mid, sum;
+        ret = right;
 
+        while (left <= right) {
+            sum = 0;
+            mid = (left + right) / 2;
+            for (int i = 0; i < n; i++) sum = sum + (mid / counter[i]);
+
+            if (sum < m) left = mid + 1;
+            else {
+                ret = mid;
+                right = mid - 1;
+            }
+        }
 
         cout << "#" << t << " " << ret << "\n";
     }
