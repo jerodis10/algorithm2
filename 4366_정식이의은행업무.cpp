8e888c22925e793ee4950dc1965c59b015125615
nth <jerodis10@naver.com>
2020-06-16 22:19:36 +0900

    진행중

--- 4366_정식이의은행업무.cpp
+++ 4366_정식이의은행업무.cpp
@@ -2,38 +2,56 @@
 #include<string>
 using namespace std;
 
+typedef long long ll;
+
 int ret;
 string n, m;
+string ori_n, ori_m;
 
-int solve(int i, int j) {
-	if (n[i] == 1) n[i] = 0;
-	else n[i] = 1;
-	if (m[j] == 1) m[j] = 0;
-	else m[j] = 1;
+int solve(int a, int b) {
+	string t[2];
+	t[0] = m;
+	t[1] = m;
+	if (n[a] == '1') n[a] = '0';
+	else n[a] = '1';
 
 	int num1 = 0, num2 = 0;
-	for (int i = n.size() - 1; i <= 0; i++) {
-		if (i == 0 && n[i] == 1) num1 += 1;
+	int n_len = n.length();
+	int m_len = m.length();
+	for (int i = 0; i < n_len; i++) {
+		if (i == n_len - 1 && n[i] == 1) num1 += 1;
 		int temp = 1;
-		if (n[i] == 1) {
-			for (int j = 0; j < i; j++) {
+		if (n[i] == '1') {
+			for (int j = i; j < n_len - 1; j++) {
 				temp *= 2;
 			}
 			num1 += temp;
 		}
 	}
-	for (int i = m.size() - 1; i <= 0; i++) {
-		if (i == 0 && n[i] == 1) num2 += 1;
-		int temp = 1;
-		if (m[i] == 1) {
-			for (int j = 0; j < i; j++) {
-				temp *= 3;
+	for (int cnt = 0; cnt < 2; cnt++) {
+		if (t[b][cnt] == '0') t[b][cnt] = '1';
+		else if (t[b][cnt] == '1') t[b][cnt] = '2';
+		else t[b][cnt] = '0';
+		for (int i = 0; i < m_len; i++) {
+			if (i == m_len - 1 && t[b][cnt] == '0') num2 += 1;
+			int temp = 1;
+			if (t[b][cnt] == '1') {
+				for (int j = i; j < m_len - 1; j++) {
+					temp *= 3;
+				}
+				num2 += temp;
 			}
-			num2 += temp;
+			if (t[b][cnt] == '2') {
+				for (int j = i; j < m_len - 1; j++) {
+					temp *= 3;
+				}
+				temp *= 2;
+				num2 += temp;
+			}
 		}
 	}
 
-	if (num1 == num2) { return 1; ret = num1; }
+	if (num1 == num2) { ret = num1; return 1; }
 	else return 0;
 }
 
@@ -46,14 +64,16 @@ int main() {
 	cin >> tc;
 	for (int t = 1; t <= tc; t++) {
 		cin >> n >> m;
-
+		ori_n = n;
+		ori_m = m;
+		ret = 0;
 		for (int i = 0; i < n.size(); i++) {
 			for (int j = 0; j < m.size(); j++) {
 				if (solve(i, j)) break;
+				n = ori_n;
+				m = ori_m;
 			}
 		}
-
-
 
 		cout << "#" << t << " " << ret << endl;
 	}
