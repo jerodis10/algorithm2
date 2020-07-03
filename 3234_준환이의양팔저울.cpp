ac243472f4e7166b92007d8456b31eb5a3a55a29
a <jerodis10@naver.com>
2020-07-03 20:57:18 +0900

    신규

--- 3234_준환이의양팔저울.cpp
+++ 3234_준환이의양팔저울.cpp
@@ -2,45 +2,32 @@
 #include <vector>
 using namespace std;
 
-int n, ret, num[9];
-int map[2][9];
-int visited[9];
-vector<int> v;
+int n, ret, sum, num[9];
+bool visited[9];
+int exp[] = { 1,2,4,8,16,32,64,128,256,512 };
+int factorial[] = { 0,1,2,6,24,120,720,5040,40320,362880 };
 
 void mem_set(void* dest, int val, int size) {
 	unsigned char* p = (unsigned char*)dest;
 	while (size--) *p++ = val;
 }
 
-void dfs(int sq) {
-	if (sq == n) {
-		int right = 0;
-		int left = 0;
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < n; j++) {
-				if (i == 0) right += map[i][j];
-				else left += map[i][j];
-			}
-		}
-		if (right <= left) ret++;
-		/*for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < n; j++) {
-				cout << map[i][j] << " ";
-			}
-			cout << endl;
-		}*/
+void dfs(int cnt, int left, int right) {
+	if (cnt == n) {
+		ret++;
 		return;
 	}
-
+	if (left >= sum - left) {
+		ret = ret + exp[n - cnt] * factorial[n - cnt];
+		return;
+	}
 	for (int i = 0; i < n; i++) {
-		if (visited[i]) continue;
-		for (int j = 0; j < 2; j++) {
-			map[j][sq] = num[i];
-			visited[i] = 1;
-			dfs(sq + 1);
-			map[j][sq] = 0;
-			visited[i] = 0;
-		}
+		if (visited[i] == true) continue;
+		visited[i] = true;
+		dfs(cnt + 1, left + num[i], right);
+
+		if (right + num[i] <= left) dfs(cnt + 1, left, right + num[i]);
+		visited[i] = false;
 	}
 }
 
@@ -53,15 +40,16 @@ int main() {
 	cin >> tc;
 	for (int t = 1; t <= tc; t++) {
 		ret = 0;
-		mem_set(map, 0, sizeof(map));
+		sum = 0;
 		mem_set(visited, 0, sizeof(visited));
+
 		cin >> n;
 		for (int i = 0; i < n; i++) {
 			cin >> num[i];
-			v.push_back(num[i]);
+			sum += num[i];
 		}
 
-		dfs(0);
+		dfs(0, 0, 0);
 
 		cout << "#" << t << " " << ret << endl;
 	}
