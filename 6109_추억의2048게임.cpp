4571c7a34c61ad1c39e431d54ab30734953213a5
a <jerodis10@naver.com>
2020-07-06 21:27:28 +0900

    신규

--- 6109_추억의2048게임.cpp
+++ 6109_추억의2048게임.cpp
@@ -1,27 +1,51 @@
 #include<iostream>
+#include<string>
+#include<cstring>
 using namespace std;
 
-int s, t, a, b, ret;
-int n, m;
+int n;
+int map[21][21];
+int dy[] = { 0,-1,0,1 };
+int dx[] = { -1,0,1,0 };
 
 void mem_set(void* dest, int val, int size) {
 	unsigned char* p = (unsigned char*)dest;
 	while (size--) *p++ = val;
 }
 
-void dfs(int cnt, int num) {
-	if (cnt > m) {
-		ret = -1;
-		return;
+void move(int y, int x, int dir) {
+	int curY = y, curX = x;
+	int ny = y, nx = x;
+	for (int i = 0; i < n - 1; i++) {
+		ny += dy[dir];
+		nx += dx[dir];
+		if (map[curY][curX] != 0) {
+			if (map[ny][nx] != 0) {
+				if (map[curY][curX] == map[ny][nx]) {
+					map[curY][curX] *= 2;
+					map[ny][nx] = 0;
+					curY += dy[dir];
+					curX += dx[dir];
+				}
+				else {
+					curY += dy[dir];
+					curX += dx[dir];
+					if (ny != curY || nx != curX) {
+						map[curY][curX] = map[ny][nx];
+						map[ny][nx] = 0;
+					}
+				}
+			}
+		}
+		else {
+			if (map[ny][nx] != 0) {
+				map[curY][curX] = map[ny][nx];
+				if (ny != curY || nx != curX) {
+					map[ny][nx] = 0;
+				}
+			}
+		}
 	}
-	if (num > t) return;
-	if (num == t) {
-		ret++;
-		return;
-	}
-
-	dfs(cnt + 1, num * a);
-	dfs(cnt + 1, num + b);
 }
 
 int main() {
@@ -33,22 +57,27 @@ int main() {
 	cin >> tc;
 	for (int testCase = 1; testCase <= tc; testCase++) {
 		n = 0;
-		m = 0;
-		ret = 0;
-
-		cin >> s >> t >> a >> b;
-
-		int temp = s;
-		while (temp <= t)
-		{
-			temp += b;
-			m++;
+		mem_set(map, 0, sizeof(map));
+		char str[20];
+		cin >> n >> str;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> map[i][j];
+			}
 		}
 
-		dfs(0, s);
-		if (ret == 0) ret = -1;
+		if (str[0] == 'l') for (int i = 0; i < n; i++) move(i, 0, 2);
+		else if (str[0] == 'r') for (int i = 0; i < n; i++) move(i, n - 1, 0);
+		else if (str[0] == 'u') for (int i = 0; i < n; i++) move(0, i, 3);
+		else if (str[0] == 'd') for (int i = 0; i < n; i++) move(n - 1, i, 1);
 
-		cout << "#" << testCase << " " << ret << endl;
+		cout << "#" << testCase << endl;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cout << map[i][j] << " ";
+			}
+			cout << endl;
+		}
 	}
 
 	return 0;
