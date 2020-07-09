768d3e7e5bc2a8c39dbadb405031f932828af881
a <jerodis10@naver.com>
2020-07-09 21:36:54 +0900

    신규

--- 6719_성수의프로그래밍강좌시청.cpp
+++ 6719_성수의프로그래밍강좌시청.cpp
@@ -1,32 +1,11 @@
 #include<iostream>
-#include<vector>
+#include<algorithm>
 using namespace std;
 
-int t, ret;
-int n, k, num[200];
-bool visited[200];
-vector<int> v;
-
-void mem_set(void* dest, int val, int size) {
-	unsigned char* p = (unsigned char*)dest;
-	while (size--) *p++ = val;
-}
-
-void dfs(int cnt) {
-	if (cnt == k) {
-
-		return;
-	}
-
-	for (int i = 0; i < n; i++) {
-		if (visited[i] == true) continue;
-		visited[i] = true;
-		v.push_back(num[i]);
-		dfs(cnt + 1);
-		v.pop_back();
-		visited[i];
-	}
-}
+int t;
+int n, k;
+int num[200];
+double ret;
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -37,14 +16,18 @@ int main() {
 	cin >> tc;
 	for (int testCase = 1; testCase <= tc; testCase++) {
 		ret = 0;
-		mem_set(visited, false, sizeof(visited));
 		cin >> n >> k;
 		for (int i = 0; i < n; i++) {
 			cin >> num[i];
 		}
 
-		dfs(0);
+		sort(num, num + n);
+		for (int i = n - k; i < n; i++) {
+			ret = (ret + num[i]) / 2;
+		}
 
+		cout << fixed;
+		cout.precision(6);
 		cout << "#" << testCase << " " << ret << endl;
 	}
 
