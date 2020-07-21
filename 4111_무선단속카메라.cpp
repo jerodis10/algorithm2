abaa3c345afb2b81469f6e33683d50d1ffff403f
a <jerodis10@naver.com>
2020-07-21 22:14:53 +0900

    풀이중

---
+++ 4111_무선단속카메라.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int t, ret;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int tc;
+	cin >> tc;
+	for (int testCase = 1; testCase <= tc; testCase++) {
+
+
+		cout << "#" << testCase << " " << ret << endl;
+	}
+
+	return 0;
+}
