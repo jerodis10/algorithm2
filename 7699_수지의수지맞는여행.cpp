5bfefa37c640e8d5f5eeebb97fbccb6314abe58b
a <jerodis10@naver.com>
2020-07-21 22:05:03 +0900

    신규

--- 7699_수지의수지맞는여행.cpp
+++ 7699_수지의수지맞는여행.cpp
@@ -1,31 +1,29 @@
 #include<iostream>
-#include<set>
 using namespace std;
 
 int t, ret;
 int r, c;
 char map[20][20];
-bool visited[20][20];
+bool alphabet[26];
 int dy[] = { 0,-1,0,1 };
 int dx[] = { -1,0,1,0 };
-set<char> s;
 
 void mem_set(void* dest, int val, int size) {
 	unsigned char* p = (unsigned char*)dest;
 	while (size--) *p++ = val;
 }
 
-void dfs(int y, int x) {
-	if (visited[y][x]) return;
-	if (s.find(map[y][x]) == s.end()) return;
-
-	visited[y][x] = true;
-	s.insert(map[y][x]);
+void dfs(int y, int x, int cnt) {
+	if (ret < cnt) ret = cnt;
 	for (int i = 0; i < 4; i++) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 		if (ny >= 0 && ny < r && nx >= 0 && nx < c) {
-			dfs(ny, nx);
+			if (alphabet[map[ny][nx] - 'A'] == false) {
+				alphabet[map[ny][nx] - 'A'] = true;
+				dfs(ny, nx, cnt + 1);
+				alphabet[map[ny][nx] - 'A'] = false;
+			}
 		}
 	}
 }
@@ -40,14 +38,16 @@ int main() {
 	for (int testCase = 1; testCase <= tc; testCase++) {
 		cin >> r >> c;
 		for (int i = 0; i < r; i++) {
-			cin >> map[i];
+			for (int j = 0; j < c; j++) {
+				cin >> map[i][j];
+			}
 		}
-		mem_set(visited, false, sizeof(visited));
 
-		s.insert(map[0][0]);
-		dfs(0, 0);
+		ret = 0;
+		mem_set(alphabet, false, sizeof(alphabet));
 
-		ret = s.size();
+		alphabet[map[0][0] - 'A'] = true;
+		dfs(0, 0, 1);
 
 		cout << "#" << testCase << " " << ret << endl;
 	}
