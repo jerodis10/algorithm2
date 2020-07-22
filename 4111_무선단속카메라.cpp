6d07b2aa681ca2454f4278e20df0729da548edb8
a <jerodis10@naver.com>
2020-07-22 21:22:21 +0900

    신규

--- 4111_무선단속카메라.cpp
+++ 4111_무선단속카메라.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int t, ret;
+int n, k;
+int num;
+vector<int> camera, gap;
+
+void solve() {
+	sort(camera.begin(), camera.end());
+	for (int i = 1; i < camera.size(); i++) {
+		int diff = camera[i] - camera[i - 1];
+		gap.push_back(diff);
+	}
+
+	sort(gap.begin(), gap.end());
+	int size = gap.size();
+	for (int i = 0; i < size - (k - 1); i++) {
+		ret += gap[i];
+	}
+}
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -11,7 +30,16 @@ int main() {
 	int tc;
 	cin >> tc;
 	for (int testCase = 1; testCase <= tc; testCase++) {
+		ret = 0;
+		camera.clear();
+		gap.clear();
+		cin >> n >> k;
+		for (int i = 0; i < n; i++) {
+			cin >> num;
+			camera.push_back(num);
+		}
 
+		solve();
 
 		cout << "#" << testCase << " " << ret << endl;
 	}
