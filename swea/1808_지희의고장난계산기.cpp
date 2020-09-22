fed339087ab886919e85028bebce24e8a71d4eba
a <jerodis10@naver.com>
2020-09-22 22:36:40 +0900

    신규

--- swea/1808_지희의고장난계산기.cpp
+++ swea/1808_지희의고장난계산기.cpp
@@ -1,66 +1,87 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+#include <stdio.h>
+#include <memory.h>
 
-int t, ret, x;
-int num[10];
-int maxN = 0;
-bool visited[10];
-vector<int> selected;
+int number[10];
+int x;
+int len;
+int datas[1000001];
 
-int select(int m, int index, int cnt) {
-	int sum = 0;
-	if (cnt == maxN) {
-		int cal = 1;
-		for (int i = selected.size() - 1; i >= 0; i--) {
-			if (i == selected.size() - 1) sum += selected[i];
-			else {
-				cal *= 10;
-				sum += (selected[i] * cal);
-			}
+void createnum(int num, int cnt)
+{
+    if (cnt > len)//입력된 자릿수보다 긴 숫자는 안만듬
+    {
+        return;
+    }
+    if (num > x)//만들고 있는 숫자가 계산하는 숫자보다 커져도 안만듬
+    {
+        return;
+    }
+    if (cnt > 0)
+    {
+        datas[num] = cnt;
+    }
 
-		}
-		if (x > sum || (x % sum) != 0) return 0;
-		else return sum;
-	}
-
-	for (int i = index; i < m; i++) {
-		if (visited[i] == true) continue;
-		visited[i] = true;
-		selected.push_back(num[i]);
-		select(m, i, cnt + 1);
-		selected.pop_back();
-		visited[i] = false;
-	}
+    for (int i = 0; i < 10; i++)
+    {
+        if (number[i] == 1)
+        {
+            createnum(num * 10 + i, cnt + 1);
+        }
+    }
 }
+int main(void) {
+    int T;
+    setbuf(stdout, NULL);
+    scanf("%d\n", &T);
+    for (int test_case = 1; test_case <= T; test_case++)
+    {
+        memset(number, 0, sizeof(number));
+        memset(datas, 0, sizeof(datas));
+        for (int i = 0; i < 10; i++)
+            scanf("%d", &number[i]);
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+        scanf("%d", &x);
+        int temp = x;
+        len = 0;
 
-	int tc;
-	cin >> tc;
-	for (int testCase = 1; testCase <= tc; testCase++) {
-		for (int i = 0; i < 10; i++) {
-			cin >> num[i];
-			if (num[i]) {
-				num[i] = (i + 1);
-				maxN++;
-			}
-		}
-		cin >> x;
+        //x가 몇자리 숫자인지 계산
+        while (temp > 0)
+        {
+            len++;
+            temp /= 10;
+        }
+        createnum(0, 0);
 
-		int temp = x;
-		for (int j = maxN; j >= 0; --j) {
-			int sum = select(j, 0, 0);
-			if (!sum) continue;
-			temp = temp / sum;
-		}
+        temp = x;
+
+        int mini = 0;
+        int num = x;
+        if (x == 1 && number[1] == 1)
+        {
+            printf("#%d %d\n", test_case, 2);
+            continue;
+        }
+        for (int i = x; i > 1; i--)
+        {
+            while (datas[i] > 0)
+            {
+                if (num % i == 0)
+                {
+                    mini += 1 + datas[i];
+                    num /= i;
+                }
+                else
+                    break;
+            }
+            if (num == 1)
+            {
+                break;
+            }
+        }
+
+        printf("#%d %d\n", test_case, num != 1 ? -1 : mini);
 
 
-		cout << "#" << testCase << " " << ret << endl;
-	}
-
-	return 0;
+    }
+    return 0;
 }
