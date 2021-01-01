16f8dd5ddf93300db21ccaf73ed70606df957af4
a <a@192.168.1.163>
2021-01-01 17:46:49 +0900

    신규

--- swea/6731_홍익이의오델로게임.java
+++ swea/6731_홍익이의오델로게임.java
@@ -25,7 +25,7 @@ public class Solution {
         	
             for(int i=0;i<N;i++) {
             		map[i] = br.readLine();
-            }
+            } 
             
             for(int i=0;i<N;i++) {
             	for(int j=0;j<N;j++) {
